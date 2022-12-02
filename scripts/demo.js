47eda5600d3efd1b27f4e9e7341503a31650d21c
anders iversen <ande397c@stud.kea.dk>
2022-12-02 12:09:50 +0100

    Implemented login page + general styling

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,31 +1,61 @@
 const dropdownElements = document.querySelectorAll(".dropdown_element h4");
-// const dropdownElementsSpan = document.querySelectorAll(".dropdown_element span");
+
+const dropdownDivs = document.querySelectorAll(".dropdown_element");
+
+const dropDownNumberBtn = document.querySelector("#dropdown_number");
+
+let dropDownCounter = 3;
 
 window.addEventListener("load", setup);
 
 function setup() {
-  console.log("setup");
-  appendDropdowns();
-}
-
-function appendDropdowns() {
   dropdownElements.forEach((dropdownElement) => {
     dropdownElement.addEventListener("click", toggleDropdown);
   });
+
+  for (i = 0; i < dropDownCounter; i++) {
+    dropdownDivs[i].classList.remove("hide");
+  }
+
+  dropDownNumberBtn.addEventListener("click", changeDropDownNumber);
 }
 
 function toggleDropdown(e) {
   const clickedElement = e.target.parentElement;
-  const dropdownElementsSpan = e.target.previousSibling.nextSibling.childNodes[1];
+  // const dropdownElementsSpan = e.target.previousSibling.nextSibling.childNodes[1];
 
   clickedElement.classList.toggle("test");
-
-  console.log(dropdownElementsSpan);
 
   let siblings = getSiblings(clickedElement);
 
   for (i = 0; i < siblings.length; i++) {
     siblings[i].classList.remove("test");
+  }
+}
+
+function changeDropDownNumber() {
+  dropDownNumberBtn.classList.toggle("activate");
+
+  if (dropDownNumberBtn.classList.contains("activate")) {
+    increaseDropDowns();
+  } else {
+    decreaseDropDowns();
+  }
+}
+
+function decreaseDropDowns() {
+  dropDownNumberBtn.textContent = "Vis flere funktioner";
+  dropDownCounter = 3;
+  for (i = 0; i < dropDownCounter; i++) {
+    dropdownDivs[i].classList.add("hide");
+  }
+}
+
+function increaseDropDowns() {
+  dropDownNumberBtn.textContent = "Vis færre funktioner";
+  dropDownCounter = dropdownDivs.length;
+  for (i = 0; i < dropDownCounter; i++) {
+    dropdownDivs[i].classList.remove("hide");
   }
 }
 
